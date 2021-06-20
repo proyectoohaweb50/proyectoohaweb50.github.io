06e2466ea26889285d5c8d30a0b5e8800698aca0
proyectoohaweb50 <86202040+proyectoohaweb50@users.noreply.github.com>
2021-06-20 18:20:39 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDzZrvhN0rOvwkpMK38N6OVRR4XEbjbcF0",
+    authDomain: "bdaguassweb50.firebaseapp.com",
+    projectId: "bdaguassweb50",
+    storageBucket: "bdaguassweb50.appspot.com",
+    messagingSenderId: "464327029477",
+    appId: "1:464327029477:web:21ad8f1e6b149dbf6c1a0c",
+    measurementId: "G-SLXBHLSF7S"
+  });
