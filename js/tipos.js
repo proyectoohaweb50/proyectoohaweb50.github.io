8768410ceede03b5be9256b122997c1a00d81d6f
proyectoohaweb50 <86202040+proyectoohaweb50@users.noreply.github.com>
2021-06-20 22:46:35 -0500

    Create tipos.js

---
+++ js/tipos.js
@@ -0,0 +1,25 @@
+/**
+ * @typedef {Object} Rol
+ * @property {string} descripción
+ */
+
+/**
+ * @typedef {Object} Reparidor // Alumno = Repartidor
+ * @property {string} nombre
+ */
+
+/**
+ * @typedef {Object} Usuario
+ * @property {string[]} rolIds
+ * @property {string} repartidorId  
+ */
+
+// @ts-nocheck
+/**
+ * @typedef {Object} Mensaje
+ * @property {string} usuarioId
+ * @property {string} texto
+ * @property {firebase.firestore.Timestamp} timestamp
+ */
+
+ export const __tipos = 0;
